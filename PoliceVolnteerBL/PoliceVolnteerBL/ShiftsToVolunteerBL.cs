67ed83fa08bee2506a6ccb05cb4172c9d9c9e315
DvirOsher <dvir1231@gmail.com>
2017-10-18 15:46:18 +0300

    started the consturctor of ShiftToVolunteerBL

--- PoliceVolnteerBL/PoliceVolnteerBL/ShiftsToVolunteerBL.cs
+++ PoliceVolnteerBL/PoliceVolnteerBL/ShiftsToVolunteerBL.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PoliceVolnteerDAL;
+using System.Data;
+using System.Data.OleDb;
 
 namespace PoliceVolnteerBL
 {
     public class ShiftsToVolunteerBL
     {
+        //לבדוק
         public List<ShiftToVolunteerBL> ShiftToVolunteerList { get; set; }
 
-        
+        public ShiftsToVolunteerBL()
+        {
+            this.ShiftToVolunteerList = new List<ShiftToVolunteerBL>();
+            DataRowCollection drc = ShiftsToVolunteerDAL.GetTable().Tables[0].Rows;
+            for (int i = 0; i < drc.Count; i++)
+            {
+                ShiftToVolunteerList.Add(new ShiftToVolunteerBL((int)drc[i]["ShiftCode"], (string)drc[i]["PhoneNumber"]));
+            }
+        }
     }
 }
