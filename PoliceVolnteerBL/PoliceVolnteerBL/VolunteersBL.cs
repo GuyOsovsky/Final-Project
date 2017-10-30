445e379814acde938cc4aab1f2c024f1aa3f656a
GuyOsovsky <guyosovsky@gmail.com>
2017-10-30 14:41:24 +0200

    More merge fixes

--- PoliceVolnteerBL/PoliceVolnteerBL/VolunteersBL.cs
+++ PoliceVolnteerBL/PoliceVolnteerBL/VolunteersBL.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using PoliceVolnteerDAL;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 
@@ -8,6 +10,13 @@ namespace PoliceVolnteerBL
     public class VolunteersBL
     {
         public List<VolunteerBL> VolunteerList { get; set; }
+        public VolunteersBL()
+        {
+            this.VolunteerList = new List<VolunteerBL>();
+
+            foreach (DataRow row in VolunteerInfoDAL.GetTable().Tables[0].Rows)
+                VolunteerList.Add(new VolunteerBL(row["PhoneNumber"].ToString()));
+        }
 
     }
 }
