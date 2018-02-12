14bf948e720dec32cf9b6a5826a2b0f920f80599
Guy <Guy@DESKTOP-AOI7D6B>
2018-02-12 13:01:02 +0200

    deleted a useless button

--- PoliceVolunteerUI/PoliceVolunteerUI/UserSettingsUI.aspx.designer.cs
+++ PoliceVolunteerUI/PoliceVolunteerUI/UserSettingsUI.aspx.designer.cs
@@ -20,14 +20,5 @@ namespace PoliceVolunteerUI {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView UserInformation;
-        
-        /// <summary>
-        /// TableButton control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button TableButton;
     }
 }
